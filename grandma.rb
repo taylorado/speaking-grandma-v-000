9752f471eb4121eebe2bb7d9e29fe2602ea6ae31
Taylor Donlan <taylordonlan@gmail.com>
2018-02-19 20:36:49 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -17,9 +17,9 @@ def speak_to_grandma(phrase)
   if phrase=="I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   end
-if phrase != phrase.upcase
+elsif phrase != phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
-  end
+  
 else
   return "NO, NOT SINCE 1938!"
 
