f24dbc11cd5f55844f9a229619568b1aaa0f6ad7
Taylor Donlan <taylordonlan@gmail.com>
2018-02-19 20:29:53 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
@@ -14,6 +14,6 @@
 
 
 def speak_to_grandma(phrase)
-  if phrase="I LOVE YOU GRANDMA!"
+  if phrase=="I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
 end
